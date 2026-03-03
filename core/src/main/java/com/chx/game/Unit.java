75e36f4a5bb3d53f204c05758f773173c800f0bb
Jinjie Chen <jinjiechen25@gmail.com>
2026-03-03 19:28:45 +0100

    尝试增加攻击功能对应的atribut

--- core/src/main/java/com/chx/game/Unit.java
+++ core/src/main/java/com/chx/game/Unit.java
@@ -1,23 +1,45 @@
 package com.chx.game;
 
 public class Unit {
+    // Position and movement
     public float x, y;
     public float speed;
-    public float scope;
+
     public boolean isEnemy;
-    public boolean isBlocked;
+
+    // Aboout atack
+    public float scope;
+    public int hp;
+    public int atk;
+    public int attakCoolDown;
+    public int attckTimer;
 
     public Unit(float x, float y, float speed, boolean isEnemy) {
         this.x = x;
         this.y = y;
         this.speed = speed;
         this.isEnemy = isEnemy;
+
+        this.hp = 10;
+        this.atk = 2;
+        this.attakCoolDown = 1;
+        this.attckTimer = 0;
+
+
     }
 
     public void update(float delta,boolean blocked) {
         if (!blocked){
             if (isEnemy) x += speed * delta;
             else x -= speed * delta;
+        }
+    }
+
+    public void attack(float delta,Unit target){
+        this.attckTimer += delta;
+        if(attckTimer >= attakCoolDown){
+            target.hp -= atk;
+            attckTimer = 0;
         }
     }
 }
